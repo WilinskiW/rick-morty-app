bb4d6e072749f9f12b37f51efc32ac5f333af58e
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-01-12 12:51:24 +0100

    initialize ui module

---
+++ rick-morty-ui/src/main/java/com/rick_morty/rick_morty_ui/RickMortyUiApplication.java
@@ -0,0 +1,13 @@
+package com.rick_morty.rick_morty_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickMortyUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickMortyUiApplication.class, args);
+	}
+
+}
---
+++ rick-morty-ui/src/test/java/com/rick_morty/rick_morty_ui/RickMortyUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rick_morty.rick_morty_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickMortyUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
