25cd9da8a9bd179cd6dfa35a1c83ff55573e7da7
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-01-13 19:39:53 +0100

    fix

--- rick-morty-web-api/src/main/java/com/rick_morty/rick_morty_web_api/api/mapper/LocationMapper.java
+++ rick-morty-web-api/src/main/java/com/rick_morty/rick_morty_web_api/api/mapper/LocationMapper.java
@@ -1,7 +1,9 @@
 package com.rick_morty.rick_morty_web_api.api.mapper;
 
 import com.rick_morty.rick_morty_data.model.Location;
+import com.rick_morty.rick_morty_web_api.api.contract.CharacterSummaryDto;
 import com.rick_morty.rick_morty_web_api.api.contract.LocationDto;
+import com.rick_morty.rick_morty_web_api.api.contract.LocationSummaryDto;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -19,12 +21,22 @@ public class LocationMapper implements Mapper<Location, LocationDto> {
                 residentsToDto(location));
     }
 
-    private List<LocationDto.Residents> residentsToDto(Location location) {
+    private List<CharacterSummaryDto> residentsToDto(Location location) {
         return location.getCurrentCharacters().stream()
-                .map(c -> new LocationDto.Residents(c.getSourceId(), c.getName()))
+                .map(character -> new CharacterSummaryDto(character.getId(),character.getName(),
+                        character.getStatus(), character.getSpecies(), character.getType(), character.getGender(),
+                        locationSummaryDto(character.getOrigin()),
+                        locationSummaryDto(character.getLocation()),
+                        character.getImageUrl()))
                 .toList();
     }
 
+    private LocationSummaryDto locationSummaryDto(Location location) {
+        if(location == null) {
+            return null;
+        }
+        return new LocationSummaryDto(location.getId(), location.getName(), location.getType(), location.getDimension());
+    }
 
     @Override
     public Location dtoToEntity(LocationDto locationDto) {
