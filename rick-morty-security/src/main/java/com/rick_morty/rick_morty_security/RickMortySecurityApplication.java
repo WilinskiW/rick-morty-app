4fab6a500eb456e619364db80678bf5131f38739
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-01-21 12:50:30 +0100

    initialize security module

---
+++ rick-morty-security/src/main/java/com/rick_morty/rick_morty_security/RickMortySecurityApplication.java
@@ -0,0 +1,13 @@
+package com.rick_morty.rick_morty_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickMortySecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickMortySecurityApplication.class, args);
+	}
+
+}
