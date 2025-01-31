b839300f5537d8ed6d70a0d735bfc276da6ed250
Wiktor Wilinski <wilinskiwiktor.ww@gmail.com>
2025-01-31 11:49:03 +0100

    add exception handler when entity already exists

--- rick-morty-web-api/src/main/java/com/rick_morty/rick_morty_web_api/api/exception/ControllersExceptionHandler.java
+++ rick-morty-web-api/src/main/java/com/rick_morty/rick_morty_web_api/api/exception/ControllersExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.rick_morty.rick_morty_web_api.api.exception;
 
+import jakarta.persistence.EntityExistsException;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,11 @@ public class ControllersExceptionHandler extends ResponseEntityExceptionHandler 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ErrorResponseObject> handleValidationException (Exception ex){
         return getErrorResponse(ex, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityExistsException.class)
+    public ResponseEntity<ErrorResponseObject> handleEntityExistsException (Exception ex){
+        return getErrorResponse(ex, HttpStatus.CONFLICT);
     }
 
     private ResponseEntity<ErrorResponseObject> getErrorResponse(Exception ex, HttpStatus status){
