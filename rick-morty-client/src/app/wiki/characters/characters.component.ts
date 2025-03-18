69f32fb7cbc64e4d1e1407774b988d4b1a8ae2aa
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2025-03-18 22:32:32 +0100

    create generic content-table

--- rick-morty-client/src/app/wiki/characters/characters.component.ts
+++ rick-morty-client/src/app/wiki/characters/characters.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
+import { ContentTableComponent } from '../../shared/content-table/content-table.component';
 
 @Component({
   selector: 'app-characters',
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    ContentTableComponent
   ],
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.css'
