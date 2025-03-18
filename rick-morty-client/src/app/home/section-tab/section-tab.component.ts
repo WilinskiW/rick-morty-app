e1f996b589f03c3e09a98e6200247d3981ce8800
Wiktor Wilinski <wilinskiwiktor2005@gmail.com>
2025-03-18 21:41:22 +0100

    Start implementing routing and shared navbar

--- rick-morty-client/src/app/home/section-tab/section-tab.component.ts
+++ rick-morty-client/src/app/home/section-tab/section-tab.component.ts
@@ -1,8 +1,11 @@
 import {Component, computed, input} from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-section-tab',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './section-tab.component.html',
   styleUrl: './section-tab.component.css'
 })
---
+++ rick-morty-client/src/app/wiki/characters/characters.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-characters',
+  imports: [
+    NavbarComponent
+  ],
+  templateUrl: './characters.component.html',
+  styleUrl: './characters.component.css'
+})
+export class CharactersComponent {
+
+}
